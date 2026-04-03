65beaa14a9cf51b6a323981f93972c2571845db0
Nykko Vanguardia <nykkovanguardia.dev@gmail.com>
2026-04-03 21:28:23 +0800

    feat: add basic foundation of lexer and handle numbers

--- crates/ledger-lexer/src/lib.rs
+++ crates/ledger-lexer/src/lib.rs
@@ -1,8 +1,9 @@
-pub enum Token {
+pub enum Token<'a> {
     //lits
     IntLiteral(i64),
     FloatLiteral(f64),
     StringLiteral(String),
+    InterpolatedString(String),  // for ``
     BoolLiteral(bool),
     CharacterLiteral(char),
 
@@ -40,7 +41,7 @@ pub enum Token {
     Bnot, // bnot
 
     //identifiers
-    Identifier(String),
+    Identifier(&'a str),
 
     //symbols
     Semicolon,
@@ -99,6 +100,7 @@ pub enum Token {
     Char,
     Bool,
     Void,
+    Null,
 
     //Reserved
     Autofree, // debating if i should add this, autofree is only for class variables, will autofree
@@ -112,13 +114,124 @@ pub enum Token {
     EOF,
 }
 
-pub struct Lexer {
-    pub source: String,
-    pub current: usize,
-    pub line: usize,
-    pub col: usize,
+//TODO add line and column of error
+#[derive(thiserror::Error, Debug)]
+pub enum LexError {
+    #[error("Float literal must have a digit after the decimal point")]
+    InvalidFloat,
+    #[error("Unexpected character: '{0}'")]
+    UnexpectedCharacter(char),
 }
 
-impl Lexer {
+pub struct Lexer<'a> {
+    source: &'a [u8],
+    current: usize,
+    line: usize,
+    col: usize,
+}
 
+impl<'a> Lexer<'a> {
+    pub fn new(input: &'a str,) -> Self {
+        Lexer { source: input.as_bytes(), current: 0, line: 1, col: 1 }
+    }
+
+    pub fn consume_next_token(&mut self) -> Result<Token<'a>, LexError> {
+        while  Self::is_whitespace(self.source[self.current]){
+            self.advance_current(); 
+        }
+        let current = self.peek_current();
+        
+        let token = match current {
+            b'\0' => Token::EOF,
+            b'0'..=b'9' => self.handle_numbers()?,
+            _ => self.handle_keywords()
+        };
+
+        Ok(token)
+    }
+
+    fn handle_keywords(&mut self) -> Token<'a> {
+        Token::Identifier("wazzup")
+    }
+
+    //TODO: Could just parse string of numbers to float itself
+    fn handle_numbers(&mut self) -> Result<Token<'a>, LexError> {
+        let mut number: i64 = 0;
+        while Self::is_number(self.peek_current()) {
+            let next_digit = (self.peek_current() - b'0') as i64;
+            number = number * 10 + next_digit; 
+            self.advance_current();
+        };
+
+        if self.peek_current() == b'.' {
+            return self.handle_float(number);
+        }
+
+        Ok(Token::IntLiteral(number))
+    }
+
+    fn handle_float(&mut self, left_of_decimal: i64) -> Result<Token<'a>, LexError> {
+        self.advance_current();
+        if !Self::is_number(self.peek_current()) {
+            return Err(LexError::InvalidFloat)
+        };
+
+        let mut right_of_decimal = 0.0;
+        let mut place = 10.0; //this is currently at the tenths place of the decimal, aka 0.X
+        while Self::is_number(self.peek_current()) {
+            let next_digit: f64 = (self.peek_current() - b'0') as f64 / place;
+            right_of_decimal = right_of_decimal + next_digit;
+            place = place * 10.0;
+            self.advance_current();
+        };
+
+        let float = left_of_decimal as f64 + right_of_decimal;
+        Ok(Token::FloatLiteral(float))
+    }
+
+    fn advance_current(&mut self) {
+        let current_byte = self.peek_current();
+        self.current += Lexer::get_char_size(current_byte);
+    }
+
+    fn peek_current(&self) -> u8 {
+        self.peek_till(0)
+    }
+
+    fn peek_next(&self) -> u8 {
+        self.peek_till(1)
+    }
+
+    fn peek_till(&self, till: u8) -> u8 {
+        if self.current >= self.source.len() {
+            return b'\0';
+        };
+
+        self.source[self.current + till as usize]
+    }
+
+    fn is_whitespace(char_in_bytes: u8) -> bool {
+        char_in_bytes == b'\n' || 
+            char_in_bytes == b'\t' || 
+            char_in_bytes == b'\r' ||
+            char_in_bytes == b' '
+    }
+
+    fn is_number(char_in_bytes: u8) -> bool {
+        matches!(char_in_bytes, b'0'..=b'9')
+    }
+
+    fn get_char_size(byte: u8) -> usize {
+        if byte < 128 {
+            1
+        } else if byte & 0b1110_0000 == 0b1100_0000 {
+            2
+        } else if byte & 0b1111_0000 == 0b1110_0000 {
+            3
+        } else if byte & 0b1111_1000 == 0b1111_0000 {
+            4
+        } else {
+            0 
+        }
+    }
 }
