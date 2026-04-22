e8f72e8e0e15cbbe2ff1ed01292d1c7b1985af7e
Nykko Vanguardia <nykkovanguardia.dev@gmail.com>
2026-04-22 23:27:36 +0800

    fix: fix match statement ordering, add unit tests

--- crates/ledger-lexer/src/lib.rs
+++ crates/ledger-lexer/src/lib.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum Token<'a> {
     //lits
     IntLit(&'a str),
@@ -158,7 +159,7 @@ pub enum Token<'a> {
 }
 
 //TODO add line and column of error
-#[derive(thiserror::Error, Debug)]
+#[derive(thiserror::Error, Debug, PartialEq)]
 pub enum LexError {
     #[error("Float literal must have a digit after the decimal point")]
     InvalidFloat,
@@ -204,13 +205,13 @@ impl<'a> Lexer<'a> {
         match current {
             b'\0' => Token::EOF,
             b'0'..=b'9' => self.handle_numbers(),
-            c if Self::is_non_underscore_symbol(c) => self.handle_symbol(),
+            b'/' if self.is_doc_comment() => self.handle_doc_comment(),
             b'"' => self.handle_string(b'"', Token::StringLit),
             b'`' => self.handle_string(b'`',Token::StringObjLit),
             b'f' if self.peek_next() == b'`' => 
                 self.handle_string(b'`',Token::InterpolatedStringObjLit),
             b'\'' => self.handle_char_lit(),
-            b'/' if self.is_doc_comment() => self.handle_doc_comment(),
+            c if Self::is_non_underscore_symbol(c) => self.handle_symbol(),
             _ => self.handle_letters()
         }
     }
@@ -705,5 +706,122 @@ impl<'a> Lexer<'a> {
 
     fn get_invalid(&self, lex_error: LexError) -> Token<'a> {
         Token::Invalid { error: lex_error, line: self.line, col: self.col, cause: "todo" }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_basic_lexing() {
+        let input = "let x = 5 + 10;";
+        
+        let tokens = Lexer::new(input).tokenize(); 
+
+        let expected = vec![
+            Token::Let,
+            Token::Identifier(&input[4..5]),
+            Token::Equals,
+            Token::IntLit(&input[8..9]),
+            Token::Plus,
+            Token::IntLit(&input[12..14]),
+            Token::Semicolon,
+            Token::EOF,
+        ];
+
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_complex_expression() {
+        let input = "let answer = (50 * 123) / 2;";
+        let tokens = Lexer::new(input).tokenize();
+
+        let expected = vec![
+            Token::Let,
+            Token::Identifier(&input[4..10]),
+            Token::Equals,
+            Token::LeftParen,
+            Token::IntLit(&input[14..16]),
+            Token::Star,
+            Token::IntLit(&input[19..22]),
+            Token::RightParen,
+            Token::Slash,
+            Token::IntLit(&input[26..27]),
+            Token::Semicolon,
+            Token::EOF,
+        ];
+
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_whitespace_insensitivity() {
+        let input = "let    x
+    = 
+    100   ;";
+        let tokens = Lexer::new(input).tokenize();
+
+        let expected = vec![
+            Token::Let,
+            Token::Identifier(&input[7..8]),
+            Token::Equals,
+            Token::IntLit(&input[20..23]),
+            Token::Semicolon,
+            Token::EOF,
+        ];
+
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_comments() {
+        let input = "//The path of the righteous man is
+            /*
+              beset on all sides by the inequities of the selfish and the tyranny of evil men
+             */
+
+            let: i32;
+            ";
+        let tokens = Lexer::new(input).tokenize();
+
+        let expected = vec![
+            Token::Let,
+            Token::Colon,
+            Token::I32,
+            Token::Semicolon,
+            Token::EOF,
+        ];
+
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_strings() {
+        let input = r#"let x = "let";"#; 
+        let tokens = Lexer::new(input).tokenize();
+
+        let expected = vec![
+            Token::Let,
+            Token::Identifier(&input[4..5]),
+            Token::Equals,
+            Token::StringLit(&input[9..12]),
+            Token::Semicolon,
+            Token::EOF,
+        ];
+
+        assert_eq!(tokens, expected);
+    }
+
+    #[test]
+    fn test_raw_doc_comment() {
+        let input = "/** * hello */";
+        let tokens = Lexer::new(input).tokenize();
+
+        let expected_slice = &input[3..12];
+
+        assert_eq!(tokens[0], Token::DocComment(expected_slice));
     }
 }
